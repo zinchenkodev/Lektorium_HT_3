d3ab343112449654755da54e01408ca0e28f3b3a
zinchenko1 <aswad1909@gmail.com>
2019-11-03 14:26:28 +0200

    fix code styles

--- config/routes.php
+++ config/routes.php
@@ -4,6 +4,5 @@ use App\Controller\HomeWorkController;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return function (RoutingConfigurator $routes) {
-$routes->add('list', '/home_work/list')
-->controller([HomeWorkController::class, 'list']);
+    $routes->add('list', '/home_work/list')->controller([HomeWorkController::class, 'list']);
 };
